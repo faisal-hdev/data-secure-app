b8ad5aa931736a29ad424807341536e0e92d3834
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-01-02 19:09:58 +0600

    Data recovery section design are added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Data from "./components/data/Data";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Data />
     </div>
   );
 }
---
+++ src/components/data/Data.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./DataStyle.css";
+
+const Data = () => {
+  return (
+    <div className="data">
+      <div className="container">
+        <div className="content">
+          <h2>
+            <span>Data</span> Recovery
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+            blanditiis facere cum aliquam amet voluptatum, quis veritatis
+            voluptates omnis magnam eaque ad iusto dolores autem hic temporibus.
+            Fuga, eaque tenetur iusto voluptatum repellendus officia facilis.
+          </p>
+          <div>
+            <button>Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Data;
