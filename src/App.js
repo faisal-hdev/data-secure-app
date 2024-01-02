adc8510c205b93eb3b3f13de3417e8c20b1f1c5f
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-01-02 21:05:36 +0600

    Footer section design are added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Data from "./components/data/Data";
 import Cloud from "./components/cloud/Cloud";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Data />
       <Cloud />
+      <Footer />
     </div>
   );
 }
