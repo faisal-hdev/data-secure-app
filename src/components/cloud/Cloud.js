8b7c49379a395958dedcc46a14344b93d96cd0d1
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-01-02 19:30:37 +0600

    Cloud section design are added

---
+++ src/components/cloud/Cloud.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CloudStyle.css";
+
+const Cloud = () => {
+  return (
+    <div className="cloud">
+      <div className="container">
+        <div className="content">
+          <h2>
+            <span>Cloud</span> Security
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quas,
+            aliquid cumque soluta eveniet quibusdam dignissimos ipsam ratione
+            numquam delectus odio praesentium, aspernatur incidunt a sunt rerum
+            voluptates, esse tenetur aliquam? Eligendi sequi eos cum.
+          </p>
+          <div>
+            <button>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cloud;
